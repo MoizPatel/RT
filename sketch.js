e54b0c6e8f24fc1b1946dd33ab543c830e3a9bb4
MoizPatel <77617367+MoizPatel@users.noreply.github.com>
2021-05-26 15:50:07 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,7 +1,8 @@
 //Create variables here
 var count=0;
 var play;
-var r;
+var restart;
+var play1, restart1;
 
 
 function preload()
@@ -12,18 +13,18 @@ function preload()
 function setup() {
 	createCanvas(400,400);
   
- // play = createSprite(200,260,100,100);
-  //play.shapeColor = "green";
-  //reset = createSprite(300,200,80,20);
+ play1 = createSprite(200,280,120,120);
+ restart1 = createSprite(298,184,50,20);
+
   restart = createButton(' Reset ');
   restart.position(625,225)
-  restart.mousePressed(reset);
+ // restart.mousePressed(reset);
  
   play = createButton(' Count ');
   play.position(500,280);
   play.style('width','100px');
   play.style('height','100px');
-  play.mousePressed(mouseP);
+ // play.mousePressed(mouseP);
 
  
  // r.touches(Reset);
@@ -36,14 +37,16 @@ function draw() {
    stroke(10);
    fill("black");
    text(" "+count,50,125);
+   play1.visible = false;
+   restart1.visible = false;
    //restart.touches(resetR);
  // play.touches(mouseP);
   if (touches.length > 0) { 
-    if (restart.overlapPoint(touches[0].x, touches[0].y)) { reset(); touches = [] }
+    if (restart1.overlapPoint(touches[0].x, touches[0].y)) { reset(); touches = [] }
    }
 
    if (touches.length > 0) { 
-    if (play.overlapPoint(touches[0].x, touches[0].y)) { mouseP(); touches = [] }
+    if (play1.overlapPoint(touches[0].x, touches[0].y)) { mouseP(); touches = [] }
    }
   // mouseP();
  /* if (touches(play)) {
