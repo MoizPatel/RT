cbee148637be1ecc3e1ce8bf4aadf58e96e18008
MoizPatel <77617367+MoizPatel@users.noreply.github.com>
2021-05-26 15:35:09 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -15,15 +15,15 @@ function setup() {
  // play = createSprite(200,260,100,100);
   //play.shapeColor = "green";
   //reset = createSprite(300,200,80,20);
-  r = createButton(' Reset ');
-  r.position(625,225)
- // reset.mousePressed(Reset);
+  restart = createButton(' Reset ');
+  restart.position(625,225)
+  restart.mousePressed(reset);
  
   play = createButton(' Count ');
   play.position(500,280);
   play.style('width','100px');
   play.style('height','100px');
- // play.mousePressed(mouseP);
+  play.mousePressed(mouseP);
 
  
  // r.touches(Reset);
@@ -36,8 +36,15 @@ function draw() {
    stroke(10);
    fill("black");
    text(" "+count,50,125);
-   r.touches(resetR);
-  play.touches(mouseP);
+   //restart.touches(resetR);
+ // play.touches(mouseP);
+  if (touches.length > 0) { 
+    if (restart.overlapPoint(touches[0].x, touches[0].y)) { reset(); touches = [] }
+   }
+
+   if (touches.length > 0) { 
+    if (play.overlapPoint(touches[0].x, touches[0].y)) { mouseP(); touches = [] }
+   }
   // mouseP();
  /* if (touches(play)) {
     mouseP();
@@ -59,7 +66,7 @@ function mouseP(){
  
 }
 
-function resetR(){
+function reset(){
   count=0;
 }
 
